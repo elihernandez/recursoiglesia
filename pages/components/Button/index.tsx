ebe14ebfa9badffae88189d16760f103690f3b7b
Eliezer Hernández <nhernandez@lap55.com>
2023-01-14 17:57:53 -0600

    fixed issues compile

--- pages/components/Button/index.tsx
+++ pages/components/Button/index.tsx
@@ -2,18 +2,18 @@ import { Icon } from '@iconify/react'
 import Link from 'next/link'
 
 interface Props {
-  btnLink: string
-  btnText: string
-  variant?: string
+    btnLink: string
+    btnText: string
+    variant?: string
 }
 
-export default function Button({ btnLink, btnText, variant = '' } : Props) {
-  return (
-    <Link href={btnLink} className={variant ? `cs-text_btn ${variant}` : 'cs-text_btn'}>
-      <>
-        <span>{btnText}</span>
-        <Icon icon="bi:arrow-right" />
-      </>
-    </Link>
-  )
+export default function Button({ btnLink, btnText, variant = '' }: Props) {
+    return (
+        <Link href={btnLink} className={variant ? `cs-text_btn ${variant}` : 'cs-text_btn'}>
+            <>
+                <span>{btnText}</span>
+                <Icon icon="bi:arrow-right" />
+            </>
+        </Link>
+    )
 }
--- pages/helpers/index.js
+++ pages/helpers/index.js
@@ -1,5 +1,5 @@
 export const pageTitle = (title) => {
-    return (document.title = title + ' - Creative Agency')
+    // return (document.title = title + ' - Creative Agency')
 }
 
 export default pageTitle
