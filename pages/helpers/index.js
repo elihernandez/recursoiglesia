8d373da95df8b9698736cd4989f4594ecc123baf
Eliezer Hernández <nhernandez@lap55.com>
2023-01-14 17:52:20 -0600

    fixed issues compile

--- pages/helpers/index.js
+++ pages/helpers/index.js
@@ -1,3 +1,5 @@
 export const pageTitle = (title) => {
-  return ( document.title = title + " - Creative Agency");
+    return (document.title = title + ' - Creative Agency')
 }
+
+export default pageTitle
--- pages/helpers/strings.tsx
+++ pages/helpers/strings.tsx
@@ -1,7 +1,9 @@
 export function capitalizeFirstLetter(str) {
 
-  // converting first letter to uppercase
-  const capitalized = str.charAt(0).toUpperCase() + str.slice(1)
+    // converting first letter to uppercase
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1)
 
-  return capitalized
+    return capitalized
 }
+
+export default capitalizeFirstLetter
