efa6bdd03845e1618760c539fa69b84a16b5e0f4
Eliezer Hernández <nhernandez@lap55.com>
2023-01-17 12:16:16 -0600

    add - add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'slick-carousel/slick/slick.css'
 import Layout from './layout'
 import '../styles/scss/index.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            window.gtag('config', process.env.GOOGLE_ANALYTICS, {
+                page_path: url,
+            })
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return <Layout>
         <Component {...pageProps} />
     </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
-export default function Document() {
+export default function Document(props) {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <Script
+                    async
+                    strategy="afterInteractive"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+                    `}
+                </Script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
